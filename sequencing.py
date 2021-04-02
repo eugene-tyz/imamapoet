7720b79845114a37a40a4ac733a925a2a675faf2
eugene-tyz <eugene.tanyanzhi@gmail.com>
2021-04-02 20:48:24 +0800

    added OOV token

--- sequencing.py
+++ sequencing.py
@@ -10,12 +10,21 @@ sentences = [
     'Do you think my dog is amazing?'
 ]
 
-# num_words is the maximum number of words to keep
-tokenizer = Tokenizer(num_words=100)
+# oov_token is meant to indicate Out Of Vocabulary words, they are words that are not in the corpus (the reference array of sentences)
+tokenizer = Tokenizer(num_words=100, oov_token="<OOV>")
 tokenizer.fit_on_texts(sentences)
 word_index = tokenizer.word_index
 
 sequences = tokenizer.texts_to_sequences(sentences)
 
+# print(word_index)
+# print(sequences)
+
+test_data = [
+    'i really love my dog',
+    'my dog loves my manatee'
+]
+
+test_seq = tokenizer.texts_to_sequences(test_data)
 print(word_index)
-print(sequences)
+print(test_seq)
