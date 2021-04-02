84cd678512d77b77ec4445214be479a3a4c05f53
eugene-tyz <eugene.tanyanzhi@gmail.com>
2021-04-02 20:27:47 +0800

    word processing for training

--- tokenisation.py
+++ tokenisation.py
@@ -1,3 +1,14 @@
 import tensorflow as tensorflow
 from tensorflow import keras
 from tensorflow.keras.preprocessing.text import Tokenizer
+
+sentences = [
+    'I love my dog',
+    'I love my cat'
+]
+
+# num_words is the maximum number of words to keep
+tokenizer = Tokenizer(num_words=100)
+tokenizer.fit_on_texts(sentences)
+word_index = tokenizer.word_index
+print(word_index)
