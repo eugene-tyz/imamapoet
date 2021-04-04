436f21ef020844b4458214e46231001673f023b0
eugene-tyz <eugene.tanyanzhi@gmail.com>
2021-04-04 18:12:29 +0800

    trying to debug sentiment detection

--- sentiment-model.py
+++ sentiment-model.py
@@ -1,8 +1,18 @@
-import tensorflow as tensorflow
+import json
+import numpy as np
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-import json
+
+
+vocab_size = 10000
+embedding_dim = 16
+max_length = 100
+trunc_type = 'post'
+padding_type = 'post'
+oov_tok = "<OOV>"
+training_size = 10000
 
 with open('datasets/Sarcasm_Headlines_Dataset.json', 'r') as f:
     datastore = json.load(f)
@@ -16,11 +26,50 @@ for item in datastore:
     urls.append(item['article_link'])
 
 
-tokenizer = Tokenizer(oov_token="<OOV>")
-tokenizer.fit_on_texts(sentences)
+#### Cut Up JSON Dataset into Training and Testing sets ####
+training_sentences = sentences[0:training_size]
+testing_sentences = sentences[training_size:]
+training_labels = labels[0:training_size]
+testing_labels = labels[training_size]
+
+
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+tokenizer.fit_on_texts(training_sentences)
 word_index = tokenizer.word_index
 
-sequences = tokenizer.texts_to_sequences(sentences)
-padded = pad_sequences(sequences, padding='post')
-print(padded[0])
-print(padded.shape)
+## Training Sequences ##
+training_sequences = tokenizer.texts_to_sequences(training_sentences)
+training_padded = pad_sequences(
+    training_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+## Testing Sequences ##
+testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
+testing_padded = pad_sequences(
+    testing_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+training_padded = np.array(training_padded)
+training_labels = np.array(training_labels)
+testing_padded = np.array(testing_padded)
+testing_labels = np.array(testing_labels)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(
+        vocab_size, embedding_dim, input_length=max_length),
+    tf.keras.layers.GlobalAveragePooling1D(),
+    tf.keras.layers.Dense(24, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+model.compile(loss='binary_crossentropy',
+              optimizer='adam', metrics=['accuracy'])
+# model.summary()
+
+num_epochs = 30
+history = model.fit(training_padded, training_labels, epochs=num_epochs,
+                    validation_data=(testing_padded, testing_labels), verbose=2)
+
+
+# sentence = ["granny starting to fear spiders in the garden might be real",
+#             "game of thrones season finale showing this sunday night"]
+# sequences = tokenizer.texts_to_sequences(sentence)
+# padded = pad_sequences(sequences, maxlen=max_length,
+#                        padding=padding_type, truncating=trunc_type)
+# print(model.predict(padded))
